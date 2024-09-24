762cc977259a37567dc2bd92bc5e9ed92463456d
Shubham <sk715152@gmail.com>
2024-09-24 12:55:45 +0530

    UI again

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/Profile';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
 import Header from "./components/Header";
 import Options from './components/Options';
 
